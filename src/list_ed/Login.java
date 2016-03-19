5309895b530e51a4f881200166abc1671bc9b0a2
Naruedom <dr.dap8@gmail.com>
2016-03-20 02:10:00 +0700

    วันที่ 20
    โปรแกรมหน้าเบิกไป 40%

---
+++ src/list_ed/Login.java
@@ -0,0 +1,29 @@
+
+package list_ed;
+ 
+public class Login {
+    
+    String id   = "1";
+    String user = "user1";
+    String pass = "1234";
+    String name = "ผู้ใช้";
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    
+    
+    
+}
